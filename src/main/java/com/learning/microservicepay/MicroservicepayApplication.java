3e2651d8490e3399a5cf2709279c6d99cc82f0ac
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2022-10-26 11:12:55 -0300

    "config eureka client"

--- src/main/java/com/learning/microservicepay/MicroservicepayApplication.java
+++ src/main/java/com/learning/microservicepay/MicroservicepayApplication.java
@@ -2,10 +2,12 @@ package com.learning.microservicepay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class  MicroservicepayApplication {
 
 	public static void main(String[] args) {
